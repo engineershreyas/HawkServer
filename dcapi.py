97115cddadda88402a5330cb61eea1af37ec649d
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-10-26 15:58:47 -0400

    added insert sql statement, not tested

--- dcapi.py
+++ dcapi.py
@@ -13,10 +13,10 @@ def putCrimeIfNecessary(cType, date, lat, lon):
         putCrime(cId, cType, date, lat, lon)
 
 def putCrime(cId, cType, date, lat, lon):
-    #TODO: write insert command
-    sqlCommand = ""
+    sqlCommand = "INSERT INTO " + tableName + " ( cId, cType, geo, datetime, citystate ) VALUES (`%(cId)`, `%(cType)`, `%(geo)`, `%(datetime)`, `%(citystate)`)"
     cityState = getCityState(lat, lon)
-    args = {'cId' : cId, 'datetime' : date, 'lat' : lat, 'lon' : lon, 'cType' : cType, 'citystate' : cityState}
+    geo = lat + "," + lon
+    args = {'cId' : cId, 'datetime' : date, 'geo' : geo, 'cType' : cType, 'citystate' : cityState}
     dbhelper.doOperation(sqlCommand, args, False, 0)
 
 #helper method to create id for crime
