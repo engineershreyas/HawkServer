3b7e8fd02be06d59b2c8ed948ea56bf5d363aa03
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-11-04 01:26:23 -0400

    Added testing endpoint

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import spotcrime
 app = Flask(__name__)
 
 @app.route('/')
 def square_of_distrust():
     return 'Welcome to the Square of Distrust!'
+
+@app.route('/test')
+def test_db():
+    spotcrime.getCrimeData(40.520911, -74.461223, .01)
+    return 'test'
