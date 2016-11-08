1c1bfe2fd183db7aae8e706badedc92c7496f6c4
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-11-08 15:55:44 -0500

    added get crimes and get reviews endpoints

--- app.py
+++ app.py
@@ -39,6 +39,44 @@ def login():
     else:
         return {'status' : 'ok', 'message' : 'Login successful!', 'userId' : userId}
 
+@app.route('/getCrimes'):
+def getCrimes():
+    lat = request.args.get('lat','')
+    lon = request.args.get('lon','')
+    radius = request.args.get('radius','')
+    if not checkValidString(lat) and not checkValidString(lon) and not checkValidString(radius):
+        try:
+            results = dcapi.getCrimes(float(lat), float(lon), float(radius))
+            return {'status' : 'ok', 'results' : results}
+        except ValueError:
+            return {'status' : 'error', 'message' : 'arguments were not all numbers'}
+    else:
+        return {'status' : 'error', 'messgae' : 'invalid arguments'}
+
+
+@app.route('/getReviews'):
+def getReviews():
+    lat = request.args.get('lat','')
+    lon = request.args.get('lon','')
+    radius = request.args.get('radius','')
+    if not checkValidString(lat) and not checkValidString(lon) and not checkValidString(radius):
+        try:
+            results = dcapi.getReviews(float(lat), float(lon), float(radius))
+            return {'status' : 'ok', 'results' : results}
+        except ValueError:
+            return {'status' : 'error', 'message' : 'arguments were not all numbers'}
+    else:
+        return {'status' : 'error', 'messgae' : 'invalid arguments'}
+
+
+@app.route('/getReviewsByUserId')
+def getReviewsByUserId():
+    userId = request.args.get('userId','')
+    if not checkValidString(userId):
+        return {'status' : 'error', 'message' : 'please enter a valid userId'}
+    else:
+        results = dcapi.getReviews(userId)
+        return {'status' : 'ok' , 'results' : results}
 
 @app.route('/updateGunLaws')
 def uploadGunLaws():
@@ -49,3 +87,8 @@ def uploadGunLaws():
 def test_db():
     dcapi.updateCrimes(40.520911, -74.461223, .1)
     return 'updateCrimes'
+
+def checkValidString(string):
+    if string == None or len(string) < 1:
+        retun False
+    return True
