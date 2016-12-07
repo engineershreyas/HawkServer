d2c92a285392db7cff947a32c47255db36be8b30
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-12-06 22:15:30 -0500

    updated key

--- spotcrime.py
+++ spotcrime.py
@@ -1,12 +1,12 @@
 import requests
 import dcapi
 
-baseURL = "http://api.spotcrime.com/crimes.json"
-key = "."
+baseURL = "https://api.spotcrime.com/crimes.json"
+key = "privatekeyforspotcrimepublicusers-commercialuse-877.410.1607"
 
 #USE THIS TO UPDATE OUR DATABASE NOT FOR CLIENT-SIDE DATA
 def getCrimeData(lat, lon, radius):
-    payload = {'key' : key, 'lat' : lat, 'lon' : lon, 'radius' : radius}
+    payload = {'key' : key, 'lat' : lat, 'lon' : lon, 'callback' : '', 'radius' : radius, '_' : '1481078110370'}
     r = requests.get(baseURL, params=payload)
     data = r.json()
     crimes = data['crimes']
